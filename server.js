6a1479f3184d82d379adac4965efd326b32c9b44
brandonlopes <brandonnlopes@protonmail.com>
2022-04-16 14:47:08 -0400

    Image downloading and json updating with node

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-[
+var fs = require('fs'),
+    http = require('http'),
+    https = require('https');
+
+var Stream = require('stream').Transform;
+
+const bookCovers = [
     {
       "Title": "Meditations",
       "src": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1383681793l/1168191._SX98_.jpg"
@@ -67,4 +73,41 @@
       "Title": "Walden & Civil Disobedience",
       "src": "https://www.gutenberg.org/files/205/205-h/images/cover.jpg"
     }
-  ]
+  
+  ];
+
+var downloadImageFromURL = (url, filename) => {
+    var client = http;
+    // if (url.toString().indexOf("https") === 0) client = https;
+
+    client.request(url, response => {
+        var data = new Stream();
+
+        response.on('data', chunk => {
+            data.push(chunk);
+        });
+
+        response.on('end', () => {
+            console.log(`downloading ${filename}`);
+            fs.writeFileSync(`./images/${filename}`, data.read());
+        });
+
+    }).end();
+
+};
+
+// bookCovers.forEach(book => downloadImageFromURL(book.src, `${book.Title}.jpg`));
+
+const books = JSON.parse(fs.readFileSync("Goodreads.json"));
+const bookcovers = JSON.parse(fs.readFileSync("bookCovers.json"));
+
+for (let i = 0; i < books.length; i++) {
+  for (let j = 0; j < bookcovers.length; j++) {
+    if(books[i].Title === bookcovers[j].Title)
+    books[i].src = bookcovers[j].src;    
+  }
+}
+
+console.log(books);
+
+fs.writeFileSync("Goodreads.json", JSON.stringify(books));
