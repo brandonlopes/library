d0fa831a67f0dc635874656a795add76c45fef97
brandonlopes <brandonnlopes@protonmail.com>
2022-06-28 10:39:06 -0400

    Make pic folder if it doesn't exist

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
 import json
 import requests
+import os
+
+if (not os.path.isdir("pics")): os.mkdir("pics")
 
 covers = open("bookCovers.json", "r+")
 goodReads = json.load(open("Goodreads.json", "r"))
+booksWithCovers = [book for book in goodReads if "src" in book]
 
 def downloadImageFromUrl(url, filename): 
     img = requests.get(url).content
-    open(filename, "wb").write(img)
+    open(f"pics/{filename}", "wb").write(img)
 
-booksWithCovers = [book for book in goodReads if "src" in book]
 for book in booksWithCovers: 
     downloadImageFromUrl(book["src"], f"{book['Title']}.jpg")
-
-# downloadImageFromUrl(book["src"], f"{book["Title"]}.jpg")
-# images = [downloadImageFromUrl(book.src, f"{book.Title}.jpg") for book in goodReads]
